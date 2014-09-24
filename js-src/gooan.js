3b5c5bd5e464d5db286ed6ac8c9bd3044a5fadcd
R. Andrew Ohana <andrew.ohana@gmail.com>
2014-09-23 17:33:34 -0700

    initial template

---
+++ js-src/gooan.js
@@ -0,0 +1,4 @@
+/* Replace the contents of this file with a Google Analytics code snippet
+ * if you would like to record analytics for your site.
+ */
+var ga = function(){};
---
+++ src/welcome.md
@@ -0,0 +1,3 @@
+# Welcome
+
+Add some welcoming text here. A table of contents will be placed below this.
